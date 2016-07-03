51ad86fd6644ba29d113ea6a892fccc2f61094f0
Adam East <adam.eastvold@gmail.com>
2016-07-03 15:14:08 -0500

    Added Bonus Challenge

--- script.js
+++ script.js
@@ -2,4 +2,21 @@ function hey (){
 	return console.log("Hey this works!")
 }
 
+function add (a){
+	return a + 2;
+}
+
+function multiply (a){
+	return a * 3;
+}
+
+function subtract (a){
+	return a - 4;
+}
+
 hey();
+
+add(multiply(subtract(2)));
+
+console.log("The result of my multiple function combination is " + add(multiply(subtract(2))) + ".");
+
